cdbad5128567ebd5556b81817fda549ac05d85bd
Alejandro <eadelgadom@gmail.com>
2022-03-24 19:03:01 +0000

    terminado y ejecutado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,40 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let excusa = document.querySelector("#excuse");
+  excusa.innerHTML = `<strong>${excuse}</strong>`;
+
+  function generarExcusa() {
+    return `${arregloRandom(who)} ${arregloRandom(action)} ${arregloRandom(
+      what
+    )} ${arregloRandom(when)}`;
+  }
+
+  document.getElementById("boton1").addEventListener("click", function() {
+    excusa.innerHTML = `<h2>${generarExcusa()}</h2>`;
+  });
 };
+let action = ["ate", "peed", "crushed", "broke"];
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function arregloRandom(arreglo) {
+  let max = arreglo.length;
+  let random = Math.random() * max;
+  random = Math.floor(random);
+  return arreglo[random];
+}
+
+let excuse = `${arregloRandom(who)} ${arregloRandom(action)} ${arregloRandom(
+  what
+)} ${arregloRandom(when)}`;
+
+console.log(excuse);
+console.log(`la excusa es :${excuse} `);
